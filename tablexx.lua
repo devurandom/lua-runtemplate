c202e30d1c7ae872853c30c63dac59616ac8216f
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-04-25 15:19:28 +0200

    iunion() convenience function to create union of arrays

--- tablexx.lua
+++ tablexx.lua
@@ -1,5 +1,6 @@
 local merge = require "pl.tablex".merge
 local pairmap = require "pl.tablex".pairmap
+local makeset = require "pl.tablex".makeset
 
 local tablexx = {}
 
@@ -7,12 +8,20 @@ function tablexx.union(t1, t2)
         return merge(t1, t2, true)
 end
 
+local union = tablexx.union
+
 function tablexx.intersection(t1, t2)
         return merge(t1, t2, false)
 end
 
 function tablexx.makelist(t)
 	return pairmap(function(k,v) return k end, t)
+end
+
+local makelist = tablexx.makelist
+
+function tablexx.iunion(t1, t2)
+	return makelist(union(makeset(t1), makeset(t2)))
 end
 
 return tablexx
