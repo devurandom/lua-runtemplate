115204fe3235735f2ea8ce7c4c04686d456d038e
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-08-05 11:20:29 +0200

    tablexx.makelist is obsolete: pl.tablex.keys creates a sequence from a set by discarding its values

--- tablexx.lua
+++ tablexx.lua
@@ -1,5 +1,5 @@
 local merge = require "pl.tablex".merge
-local pairmap = require "pl.tablex".pairmap
+local keys = require "pl.tablex".keys
 local makeset = require "pl.tablex".makeset
 
 local tablexx = {}
@@ -14,14 +14,8 @@ function tablexx.intersection(t1, t2)
         return merge(t1, t2, false)
 end
 
-function tablexx.makelist(t)
-	return pairmap(function(k,v) return k end, t)
-end
-
-local makelist = tablexx.makelist
-
 function tablexx.iunion(t1, t2)
-	return makelist(union(makeset(t1), makeset(t2)))
+	return keys(union(makeset(t1), makeset(t2)))
 end
 
 return tablexx
